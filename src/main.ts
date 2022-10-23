0d1b8af8a4e8ded2c3d8d3ce4ba4895bbf1d7215
高瀬英都 <>
2022-10-23 10:56:46 +0900

    headerの定義を削除

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { mdToHtmlAndJson } from "./mdConvert.js";
 import { imageMin } from "./imageConvert.js";
-import * as fs from 'fs'
 
 main ()
 
 async function main () {
   mdToHtmlAndJson()
   await imageMin()
-
-  const file = fs.readFileSync('./src/_headers', { encoding: "utf8" })
-  fs.writeFileSync('./dist/_headers', file)
 }
