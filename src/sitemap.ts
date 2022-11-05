f69ad60d73ba7d87b49a9a09ed135b67737c94d5
Eito Takase <eito01178@gmail.com>
2022-11-05 23:41:05 +0900

    update sitemap

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -1,19 +1,24 @@
 import * as fs from 'fs'
 import { blogsDirPath, getBlogDirFilePath, sitemapFilePath } from './path.js';
 
-const siteMapTemplate = `
-<?xml version=”1.0″ encoding=”UTF-8″?>
-<urlset xmlns=”https://www.sitemaps.org/schemas/sitemap/0.9/”>
+const siteMapTemplate = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+  http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
 {{urlset}}
+
 </urlset>
 `
 
 const urlTemplate = `
-<url>
-<loc>{{loc}}</loc>
-<lastmod>{{lastmod}}</lastmod>
-<changefreq>{{changefreq}}</changefreq>
-</url>
+  <url>
+    <loc>{{loc}}</loc>
+    <lastmod>{{lastmod}}</lastmod>
+    <changefreq>{{changefreq}}</changefreq>
+  </url>
 `
 
 const origin = 'https://obake.land/'
