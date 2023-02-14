94e0ca60f7c12b35f243e5b7b7ce06a452d0b5f7
Eito Takase <eito01178@gmail.com>
2023-02-14 22:19:12 +0900

    ogpの生成をvercelでするようにしたのでやめる

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { mdToHtmlAndJson } from "./mdConvert.js";
 import { imageMin } from "./imageConvert.js";
 import * as fs from 'fs'
-import { makeOgp } from "./makeOgp.js";
 import { generateSitemap } from "./sitemap.js";
 
 main ()
@@ -10,7 +9,6 @@ async function main () {
   mdToHtmlAndJson()
   generateSitemap()
   await imageMin()
-  await makeOgp()
 
   const file = fs.readFileSync('./src/_headers', { encoding: "utf8" })
   fs.writeFileSync('./dist/_headers', file)
