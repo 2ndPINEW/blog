ee3d773a50e353d264b739547667e32ff23ed45e
Eito Takase <eito01178@gmail.com>
2023-02-15 12:18:13 +0900

    画像の変換を事前にするように

--- src/imageConvert.ts
+++ src/imageConvert.ts
@@ -2,18 +2,22 @@ import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
 import imageminPngquant from 'imagemin-pngquant';
 
-export async function imageMin (): Promise<void> {
-  await imagemin(['assets/*.{jpg,png,jpeg}'], {
-		destination: 'dist/assets',
-		plugins: [
-			imageminWebp({quality: 100}),
-		]
-	})
+await imageMin()
 
-	await imagemin(['assets/*.{jpg,png,jpeg}'], {
-		destination: 'dist/assets',
+export async function imageMin (): Promise<void> {
+	// jpg画像をpngにする、jpg対応ちゃんとしたほうが良いけど困ったら考える
+	await imagemin(['assets/*.{jpg,jpeg}'], {
+		destination: 'assets/',
 		plugins: [
 			imageminPngquant({quality: [1, 1]}),
 		]
 	});
+
+	// png画像をwebpにする
+  await imagemin(['assets/*.png'], {
+		destination: 'assets/',
+		plugins: [
+			imageminWebp({quality: 100}),
+		]
+	})
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { mdToHtmlAndJson } from "./mdConvert.js";
-import { imageMin } from "./imageConvert.js";
 import * as fs from 'fs'
 import { generateSitemap } from "./sitemap.js";
 
@@ -8,7 +7,6 @@ main ()
 async function main () {
   mdToHtmlAndJson()
   generateSitemap()
-  await imageMin()
 
   const file = fs.readFileSync('./src/_headers', { encoding: "utf8" })
   fs.writeFileSync('./dist/_headers', file)
