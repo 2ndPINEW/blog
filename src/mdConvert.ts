fff18aab0f8fde54c3125fe6df60a7cab9b44453
Eito Takase <eito01178@gmail.com>
2023-04-28 23:23:36 +0900

    https://から始まる画像はsrcsetにしない

--- src/mdConvert.ts
+++ src/mdConvert.ts
@@ -1,100 +1,122 @@
-import * as fs from 'fs'
-import { marked } from 'marked'
-import { imageSize } from 'image-size'
+import * as fs from "fs";
+import { marked } from "marked";
+import { imageSize } from "image-size";
 
-import { blogsDirPath, distDirPath, getBlogDirFilePath, getDistDirFilePath, getDistPagingJsonPath, getDistTagsJsonPath, resourceUrl } from './path.js'
-
+import {
+  blogsDirPath,
+  distDirPath,
+  getBlogDirFilePath,
+  getDistDirFilePath,
+  getDistPagingJsonPath,
+  getDistTagsJsonPath,
+  resourceUrl,
+} from "./path.js";
 
 interface MetaData {
-  title: string
-  description: string
-  icon: string
-  date: string
-  tags: string[]
-  path: string
+  title: string;
+  description: string;
+  icon: string;
+  date: string;
+  tags: string[];
+  path: string;
 }
 
 /**
  * マークダウンの記事をHTMLに変換してインデックス用のJSONを作る
  */
-export function mdToHtmlAndJson (): void {
-  markedImageTagRenderer()
-  const fileNames = fs.readdirSync(blogsDirPath)
-    .filter(filePath => filePath.endsWith('.md'))
-    .map(fileName => fileName.replace('.md', ''))
+export function mdToHtmlAndJson(): void {
+  markedImageTagRenderer();
+  const fileNames = fs
+    .readdirSync(blogsDirPath)
+    .filter((filePath) => filePath.endsWith(".md"))
+    .map((fileName) => fileName.replace(".md", ""));
 
   try {
-    fs.mkdirSync(distDirPath)
+    fs.mkdirSync(distDirPath);
   } catch {}
 
-  const metaDatas: MetaData[] = []
-  const tags: string[] = []
+  const metaDatas: MetaData[] = [];
+  const tags: string[] = [];
 
-  fileNames.forEach(fileName => {
-    const blogDirFilePath = getBlogDirFilePath(fileName)
-    const distDirFilePath = getDistDirFilePath(fileName)
+  fileNames.forEach((fileName) => {
+    const blogDirFilePath = getBlogDirFilePath(fileName);
+    const distDirFilePath = getDistDirFilePath(fileName);
 
-    const file = fs.readFileSync(blogDirFilePath, { encoding: "utf8" })
+    const file = fs.readFileSync(blogDirFilePath, { encoding: "utf8" });
 
-    const data = file.split('---metadata')
+    const data = file.split("---metadata");
     if (data.length !== 2) {
-      console.error(`Metadata reade error: ${fileName}`)
-      process.exit(1)
+      console.error(`Metadata reade error: ${fileName}`);
+      process.exit(1);
     }
 
-    const metaData: MetaData = JSON.parse(data[0])
-    metaData.path = fileName
-    
-    if (!metaData.tags.includes('Draft')) {
-      metaDatas.push(metaData)
+    const metaData: MetaData = JSON.parse(data[0]);
+    metaData.path = fileName;
+
+    if (!metaData.tags.includes("Draft")) {
+      metaDatas.push(metaData);
     }
 
-    const markdownData = data[1]
-    const html = marked(markdownData)
+    const markdownData = data[1];
+    const html = marked(markdownData);
 
     const pageData = {
       metaData: metaData,
-      html: html
-    }
+      html: html,
+    };
 
-    fs.writeFileSync(distDirFilePath, JSON.stringify(pageData))
-  })
+    fs.writeFileSync(distDirFilePath, JSON.stringify(pageData));
+  });
 
-  metaDatas.forEach(metaData => {
-    metaData.tags.forEach(metaDataTag => {
-      const pushed = tags.some(tag => tag === metaDataTag)
+  metaDatas.forEach((metaData) => {
+    metaData.tags.forEach((metaDataTag) => {
+      const pushed = tags.some((tag) => tag === metaDataTag);
       if (!pushed) {
-        tags.push(metaDataTag)
+        tags.push(metaDataTag);
       }
-    })
-  })
+    });
+  });
 
-  fs.writeFileSync(getDistTagsJsonPath(), JSON.stringify( { tags }))
+  fs.writeFileSync(getDistTagsJsonPath(), JSON.stringify({ tags }));
 
-  metaDatas.reverse()
+  metaDatas.reverse();
 
-  fs.writeFileSync(getDistPagingJsonPath(0), JSON.stringify({ contents: metaDatas }))
+  fs.writeFileSync(
+    getDistPagingJsonPath(0),
+    JSON.stringify({ contents: metaDatas })
+  );
 
-  const splitArray = (array: any[], n: number) => array.reduce((a: any[], c: any[], i: number) => i % n == 0 ? [...a, [c]] : [...a.slice(0, -1), [...a[a.length - 1], c]], [])
+  const splitArray = (array: any[], n: number) =>
+    array.reduce(
+      (a: any[], c: any[], i: number) =>
+        i % n == 0 ? [...a, [c]] : [...a.slice(0, -1), [...a[a.length - 1], c]],
+      []
+    );
 
   splitArray(metaDatas, 50).forEach((datas: MetaData[], index: number) => {
-    fs.writeFileSync(getDistPagingJsonPath(index + 1), JSON.stringify({ contents: datas }))
-  })
+    fs.writeFileSync(
+      getDistPagingJsonPath(index + 1),
+      JSON.stringify({ contents: datas })
+    );
+  });
 }
 
-function markedImageTagRenderer (): void {
+function markedImageTagRenderer(): void {
   const renderer = {
     image(href: string | null, title: string | null, text: string): string {
-      if (!href) return ''
+      if (!href) return "";
 
-      const size = imageSize(`.${href}`)
-      const width = size.width
-      const height = size.height
+      if (href.startsWith("https://")) {
+        return `<img src="${href}" alt="${title ?? text}" loading="lazy">`;
+      }
 
+      const size = imageSize(`.${href}`);
+      const width = size.width;
+      const height = size.height;
 
-      const pngUrl = `${resourceUrl}${href}`
-      const webpUrl = `${resourceUrl}${href}`.replace('.png', '.webp')
-  
+      const pngUrl = `${resourceUrl}${href}`;
+      const webpUrl = `${resourceUrl}${href}`.replace(".png", ".webp");
+
       // srcset使いたいなー
       // <source width="720" height="404" type="image/webp" srcset="./img/header/big_banner-360.webp 360w, ./img/header/big_banner-480.webp 480w, ./img/header/big_banner-720.webp 720w, ./img/header/big_banner-960.webp 960w, ./img/header/big_banner-1280.webp 1280w, ./img/header/big_banner-1600.webp 1600w, ./img/header/big_banner-1920.webp 1920w">
       // <source width="720" height="404" type="image/png" srcset="./img/header/big_banner-360.png 360w, ./img/header/big_banner-480.png 480w, ./img/header/big_banner-720.png 720w, ./img/header/big_banner-960.png 960w, ./img/header/big_banner-1280.png 1280w, ./img/header/big_banner-1600.png 1600w, ./img/header/big_banner-1920.png 1920w">
@@ -103,9 +125,9 @@ function markedImageTagRenderer (): void {
           <source width="${width}" height="${height}" type="image/webp" srcset="${webpUrl}">
           <source width="${width}" height="${height}" type="image/png" srcset="${pngUrl}">
           <img src="${pngUrl}" alt="${title ?? text}" loading="lazy">
-        </picture>`
-    }
-  }
+        </picture>`;
+    },
+  };
 
-  marked.use({ renderer })
+  marked.use({ renderer });
 }
